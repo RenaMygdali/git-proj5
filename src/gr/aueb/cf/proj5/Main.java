57b86904c59f30df9c95f3c3ebd2e7ce8771aa49
Rena Migdali <rena.mygdali@hotmail.com>
2023-12-11 19:18:57 +0200

    Main & gitignore

---
+++ src/gr/aueb/cf/proj5/Main.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.proj5;
+
+/**
+ * Ένα υποθετικό project για GitHub demonstration.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello CF!!!");
+    }
+}
